e180333da9da9da72b94b7497bb7e13d0523410b
your-name <abdellamahmoud98@gmail.com>
2024-11-28 17:08:21 -0500

    completed : Coding 11.9: Feed App - Register Component

--- src/util/ApiUtil.js
+++ src/util/ApiUtil.js
@@ -1,27 +1,28 @@
-import axios from "axios";
+import axios from 'axios'
+const API_BASE_URL = 'http://localhost:8080'
 
-const API_BASE_URL = "http://localhost:8080";
 const frameToken = (token) => `Bearer ${token}`;
 const frameResponse = (
-    reqStatus = 0,
-    reqPayLoad = "Invalid request. Please try again later."
-  ) => {
-    return {
-      status: reqStatus,
-      payLoad: reqPayLoad,
-    };
-  };
-  export const registerApi = async (
-    username,
-    password,
-    emailId,
-    firstName,
-    lastName,
-    phone
-  ) => {
-    let response = frameResponse();
-      
-  };
+  reqStatus = 0,
+  reqPayLoad = 'Invalid request. Please try again later.'
+) => {
+  return {
+    status: reqStatus,
+    payLoad: reqPayLoad,
+  }
+}
+
+export const registerApi = async (
+  username,
+  password,
+  emailId,
+  firstName,
+  lastName,
+  phone
+) => {
+  let response = frameResponse()
+
+
   try {
     const url = `${API_BASE_URL}/user/signup`;
     const apiResponse = await axios.post(url, {
@@ -31,15 +32,16 @@ const frameResponse = (
       firstName,
       lastName,
       phone,
-    });
+    })
     if (apiResponse.status === 200) {
-      response = frameResponse(1);
+      response = frameResponse(1)
     }
   } catch (err) {
     if (err.response) {
-      response = frameResponse(0, err.response.data.message);
+      response = frameResponse(0, err.response.data.message)
     }
-    console.log(err);
+    console.log(err)
   } finally {
-    return response;
+    return response
   }
+}
