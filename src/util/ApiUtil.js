e42746a924181a0ccc597ba5072f89eac7c702f9
your-name <abdellamahmoud98@gmail.com>
2024-11-29 10:46:55 -0500

    added 11.12: Feed App - Login API

--- src/util/ApiUtil.js
+++ src/util/ApiUtil.js
@@ -70,3 +70,30 @@ export const verifyEmailApi = async (token) => {
     return response
   }
 }
+
+
+// this function makes an api call to http://localhost:8080/user/login/ to the back end
+
+export const loginApi = async (username, password) => {
+  //setting the insital value of status to 0 and payload to "Invalid request.please try again later."
+  let response = frameResponse();
+  try {
+    const url = `${API_BASE_URL}/user/login`;
+    const apiResponse = await axios.post(url, { username, password });
+    if (apiResponse.status === 200) {
+      const payLoad = {
+        userData: apiResponse.data,
+        token: apiResponse.headers.authorization,
+      };
+      response = frameResponse(1, payLoad);
+    }
+} catch (err) {
+    if (err.response) {
+      response = frameResponse(0, err.response.data.message);
+    }
+    console.log(err);
+} finally {
+    return response;
+}
+
+};
